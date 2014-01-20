1467fc2fc20b88fbaf699ba0716d59b12d1757e4
smikes <smikes@cubane.com>
2014-01-20 06:40:11 -0700

    add getAlgorithmVersion

--- src/node-inchi.cc
+++ src/node-inchi.cc
@@ -3,6 +3,7 @@
 #include <uv.h>
 
 #include "../INCHI-1-API/INCHI_API/inchi_dll/inchi_api.h"
+#include "../INCHI-1-API/INCHI_API/inchi_dll/mode.h"
 
 using namespace v8;
 
@@ -21,11 +22,17 @@ Handle<Value> StructureToInChI(const Arguments& args) {
 
 }
 
+Handle<Value> getAlgorithmVersion(const Arguments& args) {
+  HandleScope scope;
+  return scope.Close(String::New(INCHI_VERSION));
+}
+
 
 void init(Handle<Object> exports) {
   exports->Set(String::NewSymbol("hello"),
                FunctionTemplate::New(Method)->GetFunction());
-
+  exports->Set(String::NewSymbol("getAlgorithmVersion"),
+               FunctionTemplate::New(getAlgorithmVersion)->GetFunction());
 }
 
 NODE_MODULE(libinchi, init)
