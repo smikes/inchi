8519c9ed67f1fa89a9740fe7e28fe8eac24b7d2c
smikes <smikes@cubane.com>
2014-01-20 10:18:28 -0700

    initial rev: replace "using namespace" directive

---
+++ src/using_v8.h
@@ -0,0 +1,26 @@
+#ifndef SRC_USING_V8_H_
+#define SRC_USING_V8_H_
+/* using-v8.h
+ * Copyright 2014 Cubane Canada, Inc.
+ *
+ * Released under the MIT license -- see MIT-LICENSE for details
+ */
+
+using v8::Arguments;
+using v8::Exception;
+using v8::Function;
+using v8::FunctionTemplate;
+using v8::Handle;
+using v8::HandleScope;
+using v8::Integer;
+using v8::Number;
+using v8::kExternalUnsignedByteArray;
+using v8::Local;
+using v8::Object;
+using v8::Persistent;
+using v8::String;
+using v8::Undefined;
+using v8::V8;
+using v8::Value;
+
+#endif  // SRC_USING_V8_H_
