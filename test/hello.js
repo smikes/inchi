7dd37476138f5df4bf262fca6d3bb299f5f0c34c
smikes <smikes@cubane.com>
2014-01-20 09:31:23 -0700

    test library return codes

--- test/hello.js
+++ test/hello.js
@@ -31,5 +31,17 @@ describe('inchilib', function () {
 
             fs.readFile('package.json', afterRead);
         });
+
+        it('should publish constants for return codes', function () {
+            var inchilib = inchi.inchilib;
+
+            inchilib.inchi_Ret_OKAY.should.be.exactly(0);
+            inchilib.inchi_Ret_WARNING.should.be.exactly(1);
+            inchilib.inchi_Ret_ERROR.should.be.exactly(2);
+            inchilib.inchi_Ret_FATAL.should.be.exactly(3);
+            inchilib.inchi_Ret_UNKNOWN.should.be.exactly(4);
+            inchilib.inchi_Ret_BUSY.should.be.exactly(5);
+            inchilib.inchi_Ret_EOF.should.be.exactly(-1);
+        });
     });
 });
