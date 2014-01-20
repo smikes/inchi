f9183ddacced94b7f8eccdb074fd74184428b7d4
smikes <smikes@cubane.com>
2014-01-20 09:31:00 -0700

    add and document old-style return codes

--- src/node-inchi.cc
+++ src/node-inchi.cc
@@ -2,29 +2,26 @@
 #include <v8.h>
 #include <uv.h>
 
-#include "../INCHI-1-API/INCHI_API/inchi_dll/inchi_api.h"
-#include "../INCHI-1-API/INCHI_API/inchi_dll/mode.h"
+#include "inchi_dll/inchi_api.h"
+#include "inchi_dll/mode.h"
 
 using namespace v8;
 
 /**
- * this is the direct wrapper of the low-level C++ api
- * 
+ * Direct wrapper of the low-level C++ api
+ *
+ * http://www.inchi-trust.org/fileadmin/user_upload/software/inchi-v1.04/InChI_API_Reference.pdf
+ *
  * @module InChI
  * @class  InChILib
  */
 
 
-Handle<Value> StructureToInChI(const Arguments& args) {
-  HandleScope scope;
-  return scope.Close(String::New("world"));
-
-  // build INCHI_Input structure from args
-
-  //
-
-}
-
+/**
+ * Returns the current version of the InChI algorithm
+ * @method getAlgorithmVersion
+ * @return {String} current algorithm version (from INCHI_VERSION)
+ */
 Handle<Value> getAlgorithmVersion(const Arguments& args) {
   HandleScope scope;
   return scope.Close(String::New(INCHI_VERSION));
@@ -34,6 +31,62 @@ Handle<Value> getAlgorithmVersion(const Arguments& args) {
 void init(Handle<Object> exports) {
   exports->Set(String::NewSymbol("getAlgorithmVersion"),
                FunctionTemplate::New(getAlgorithmVersion)->GetFunction());
-}
+  
+  /**
+   * Return codes for the GetINCHI/GetStdINCHI (old-style) functions
+   * @module InChI
+   * @class RetValGetInchi
+   */
 
+  /**
+   * Success; no errors or warnings
+   * @property {int} inchi_Ret_OKAY
+   * @final
+   */
+  exports->Set(String::NewSymbol("inchi_Ret_OKAY"), Number::New(inchi_Ret_OKAY));
+
+  /**
+   * Success; warning(s) issued
+   * @property {int} inchi_Ret_WARNING
+   * @final
+   */
+  exports->Set(String::NewSymbol("inchi_Ret_WARNING"), Number::New(inchi_Ret_WARNING));
+
+  /**
+   * Error: no InCHI has been created
+   * @property {int} inchi_Ret_ERROR
+   * @final
+   */
+  exports->Set(String::NewSymbol("inchi_Ret_ERROR"), Number::New(inchi_Ret_ERROR));
+
+  /**
+   * Severe error: no InCHI has been created (typically, memory allocation failure)
+   * @property {int} inchi_Ret_FATAL
+   * @final
+   */
+  exports->Set(String::NewSymbol("inchi_Ret_FATAL"), Number::New(inchi_Ret_FATAL));
+
+  /**
+   * Unknown program error
+   * @property {int} inchi_Ret_UNKNOWN
+   * @final
+   */
+  exports->Set(String::NewSymbol("inchi_Ret_UNKNOWN"), Number::New(inchi_Ret_UNKNOWN));
+
+  /**
+   * Previous call to InChI has not returned yet
+   * @property {int} inchi_Ret_BUSY
+   * @final
+   */
+  exports->Set(String::NewSymbol("inchi_Ret_BUSY"), Number::New(inchi_Ret_BUSY));
+
+  /**
+   * no structural data has been provided
+   * @property {int} inchi_Ret_EOF
+   * @final
+   */
+  exports->Set(String::NewSymbol("inchi_Ret_EOF"), Number::New(inchi_Ret_EOF));
+
+
+}
 NODE_MODULE(libinchi, init)
