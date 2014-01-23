774a1218adec5c12297dd4a4d574049cc6ddad45
smikes <smikes@cubane.com>
2014-01-23 12:52:48 -0700

    make InchiInput not inherit from inchi_Input

--- src/inchi_input.cc
+++ src/inchi_input.cc
@@ -19,12 +19,6 @@
  * @constructor
  */
 InchiInput::InchiInput() {
-  /* can't use base initialization */
-  atom = 0;
-  stereo0D = 0;
-  szOptions = 0;
-  num_atoms = 0;
-  num_stereo0D = 0;
 }
 
 /**
