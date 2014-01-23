d56883e31f27581a4861ed358921790885d5c1f6
smikes <smikes@cubane.com>
2014-01-23 13:48:20 -0700

    declare constructor

--- src/inchi_input.h
+++ src/inchi_input.h
@@ -8,6 +8,11 @@
 
 #include <vector>
 
+#include <node.h>
+#include <v8.h>
+
+#include "./using_v8.h"
+
 #include "inchi_dll/inchi_api.h"
 
 struct InchiInput {
@@ -16,6 +21,8 @@ struct InchiInput {
 
   std::vector<inchi_Atom> atoms_;
   std::vector<inchi_Stereo0D> stereo0D_;
+
+  static InchiInput * Construct(Handle<Value> val);
 };
 
 #endif  // SRC_INCHI_INPUT_H_
