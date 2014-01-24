b9bea5f9cec5742a32a3c863a3c2e2ed450518f4
smikes <smikes@cubane.com>
2014-01-23 21:22:20 -0700

    add reasonable behavior tests

--- test/inchilib.js
+++ test/inchilib.js
@@ -32,7 +32,9 @@ describe('inchilib', function () {
         (result.auxinfo).should.startWith('AuxInfo=1/0/N:1,2/rA:2CO');
         (result.code).should.be.exactly(inchilib.inchi_Ret_OKAY);
         (result.message).should.equal(''); // no error message
-        (result.log).should.startWith('Generating standard InChI\nInput format: MOLfile\nOutput format: Plain text\nFull Aux. info\nNo timeoutUp to 1024 atoms per structure');
+        (result.log).should.startWith('Generating standard InChI\nInput format: MOLfile\n' +
+                                      'Output format: Plain text\nFull Aux. info\n' +
+                                      'No timeoutUp to 1024 atoms per structure');
     });
     it('should not leak memory when converting molecules', function() {
 
@@ -44,8 +46,48 @@ describe('inchilib', function () {
         },
             i;
 
-        for (i = 0; i < 100; i += 1) {
+        for (i = 0; i < 10; i += 1) {
             inchilib.GetINCHISync(methanol);
         }
     });
+    it('should make reasonable complaints for incorrect molecules', function () {
+        var invalid = {
+            atom: [
+                { elname: 'Foo', neighbor: [1] },
+                { elname: 'Bar', neighbor: [0] }
+            ]
+        };
+
+        var result = inchilib.GetINCHISync(invalid);
+
+        (result.inchi).should.be.exactly('');
+        (result.auxinfo).should.be.exactly('');
+        (result.code).should.be.exactly(inchilib.inchi_Ret_ERROR);
+        (result.message).should.equal('Unknown element(s): Foo');
+    });
+    it('should not crash on empty molecule', function () {
+        var empty = {
+            atom: [
+            ]
+        };
+
+        var result = inchilib.GetINCHISync(empty);
+
+        (result.inchi).should.be.exactly('');
+        (result.auxinfo).should.be.exactly('');
+        (result.code).should.be.exactly(inchilib.inchi_Ret_EOF);
+        (result.message).should.equal('');
+    });
+    it('should not crash on malformed input', function () {
+        var noAtoms = {
+        };
+
+        var result = inchilib.GetINCHISync(noAtoms);
+
+        (result.inchi).should.be.exactly('');
+        (result.auxinfo).should.be.exactly('');
+        (result.code).should.be.exactly(inchilib.inchi_Ret_EOF);
+        (result.message).should.equal('');
+    });
+
 });
