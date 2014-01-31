d1632d306ca03f8ec2048ada6199e069094f1090
smikes <smikes@cubane.com>
2014-01-31 06:12:26 -0700

    internal: rename inchi_input to molecule

---
+++ src/molecule.h
@@ -0,0 +1,46 @@
+#ifndef SRC_MOLECULE_H_
+#define SRC_MOLECULE_H_
+/* molecule.h
+ * Copyright 2014 Cubane Canada, Inc.
+ *
+ * Released under the MIT license -- see MIT-LICENSE for details
+ */
+
+#include <node.h>
+#include <v8.h>
+
+#include <vector>
+
+#include "./using_v8.h"
+
+#include "inchi_dll/inchi_api.h"
+
+#include "./inchi_atom.h"
+#include "./inchi_bond.h"
+#include "./get_inchi_data.h"
+
+struct InchiInput {
+  InchiInput();
+  ~InchiInput();
+
+  std::vector<InchiAtom> atoms_;
+  std::vector<InchiBond> bonds_;
+  std::vector<inchi_Stereo0D> stereo0D_;
+
+  /* native api */
+  int GetInchi();
+
+  int addAtom(const char * element);
+  InchiAtom& getAtom(int i);
+
+  void addBond(const InchiBond& b);
+
+  GetINCHIData * tearOffGetINCHIData();
+
+  static InchiInput * Create(Handle<Value> val);
+};
+
+Handle<Object> GetResult(GetINCHIData * data);
+
+
+#endif  // SRC_MOLECULE_H
