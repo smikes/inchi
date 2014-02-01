edc72ee171b5c2a0d2a68d3b43e92c52fbce6bed
Sam Mikes <smikes@cubane.com>
2014-02-01 13:59:04 +0000

    atom: remove scratch implementation

--- src/atom.h
+++ src/atom.h
@@ -5,20 +5,4 @@
  *
  * Released under the MIT license -- see MIT-LICENSE for details
  */
-
-#include <nan.h>
-
-#include "./inchi_atom.h"
-
-struct Atom : public node::ObjectWrap {
-  Atom(const char * name = "");
-
-  static void Init(v8::Handle<v8::Object> exports);
-
-  static NAN_METHOD(New);
-  static NAN_METHOD(GetName);
-  static NAN_METHOD(SetName);
-
-  InchiAtom atom;
-};
 #endif  // SRC_ATOM_H_
