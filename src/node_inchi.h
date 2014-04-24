7e1695f00c77d2cad5cc4c98de2650718602ec66
Sam Mikes <smikes@cubane.com>
2014-04-24 21:37:43 +0100

    meta: use >1.0 cppunitlite

--- src/node_inchi.h
+++ src/node_inchi.h
@@ -1,5 +1,10 @@
 #ifndef SRC_NODE_INCHI_H_
 #define SRC_NODE_INCHI_H_
+/* node_inchi.h
+ * Copyright 2014 Cubane Canada, Inc.
+ *
+ * Released under the MIT license -- see MIT-LICENSE for details
+ */
 
 const InchiAtom InchiAtom_makeFromObject(Handle<Object> atom);
 
